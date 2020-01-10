6fc5a658024ac741c0e817643c864c34a3f5f4a0
Danila Balagansky <d.balagansky@me.com>
2020-01-10 23:06:02 +0300

    Send email to watchers on issue creation

--- lib/redmine_category_watcher/hooks.rb
+++ lib/redmine_category_watcher/hooks.rb
@@ -1,14 +1,8 @@
-
 module RedmineCategoryWatchers
   class Hooks < Redmine::Hook::ViewListener
-    #  def controller_issues_new_before_save(context)
-    #    context[:issue][:mail] = User.current.mail unless User.current == nil
-    #  end
-    def controller_issues_new_after_save(context)
-    issue = Issue.find(context[:issue].id)
-      auto_watch issue
-      issue.save
-    end
+        def controller_issues_new_before_save(context)
+      auto_watch context[:issue]
+        end
     def auto_watch(issue)
       unless issue.category_id.nil?
         cw = CategoryWatcher.where(category_id:issue.category_id).first
